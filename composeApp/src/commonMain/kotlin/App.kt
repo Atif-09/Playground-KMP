6938ed38961880c933ccb7936818519de2e3987d
Muhammad Atif <ma2669637@gmail.com>
2024-03-23 21:18:40 +0500

    initial commit

---
+++ composeApp/src/commonMain/kotlin/App.kt
@@ -0,0 +1,27 @@
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import functionalities.DockedSearchBarSample
+import org.jetbrains.compose.resources.ExperimentalResourceApi
+import org.jetbrains.compose.ui.tooling.preview.Preview
+
+@OptIn(ExperimentalResourceApi::class)
+@Composable
+@Preview
+fun App() {
+    MaterialTheme {
+        DockedSearchBarSample()
+       /* var showContent by remember { mutableStateOf(false) }
+        Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
+            Button(onClick = { showContent = !showContent }) {
+                Text("Click me!")
+            }
+            AnimatedVisibility(showContent) {
+                val greeting = remember { Greeting().greet() }
+                Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
+                    Image(painterResource(Res.drawable.compose_multiplatform), null)
+                    Text("Compose: $greeting")
+                }
+            }
+        }*/
+    }
+}
