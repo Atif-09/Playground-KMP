274e66aaa7d53e291304eaa63d14301f79e1ec6b
Muhammad Atif <ma2669637@gmail.com>
2024-03-27 14:56:30 +0500

    mouse events for compose desktop

--- composeApp/src/desktopMain/kotlin/main.kt
+++ composeApp/src/desktopMain/kotlin/main.kt
@@ -1,8 +1,40 @@
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.PointerEventType
+import androidx.compose.ui.input.pointer.onPointerEvent
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 
 fun main() = application {
     Window(onCloseRequest = ::exitApplication, title = "PlaygroundKMP") {
-        App()
+    MouseEvents()
+    //App()
     }
+}
+
+@OptIn(ExperimentalComposeUiApi::class)
+@Composable
+fun MouseEvents() {
+    var color by remember { mutableStateOf(Color(0, 0, 0)) }
+    Box(
+        modifier = Modifier
+            .wrapContentSize(Alignment.Center)
+            .fillMaxSize()
+            .background(color = color)
+            .onPointerEvent(PointerEventType.Move) {
+                val position = it.changes.first().position
+                color = Color(position.x.toInt() % 256, position.y.toInt() % 256, 0)
+            }
+    )
 }
